8227cfeccd2438e51fee99bb042ff794adb81a4d
kbyrd <kbyrd@outlook.com>
2017-09-27 18:12:27 -0400

    adding the array.prototype.reduce and map impl

--- module4/script.js
+++ module4/script.js
@@ -36,6 +36,20 @@ WARNING!!! WARNING!!!
 (function(){
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	
+	function salutations(element, helloFunction, byeFunction){
+		var firstLetter = element.toLowerCase().charAt(0);
+		
+		if(typeof helloFunction === "function" && typeof byeFunction === "function"){
+			if ("j" === firstLetter) {
+				return byeFunction(element);
+			}else {
+				return helloFunction(element);
+			}
+		}else{
+			salutations(element, helloSpeaker.speak, byeSpeaker.speak);
+		}
+	}
+	
 	// STEP 10:
 	// Loop over the names array and say either 'Hello' or "Good Bye"
 	// using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -50,17 +64,44 @@ WARNING!!! WARNING!!!
 		  // string object's 'toLowerCase' method on the result so we can compare
 		  // to lower case character 'j' afterwards.
 		  // Look up these methods on Mozilla Developer Network web site if needed.
-		  var firstLetter = names[i].toLowerCase().charAt(0);
+//		  var firstLetter = names[i].toLowerCase().charAt(0);
 		
 		  // STEP 12:
 		  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
 		  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
 		  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
 		  // name in the loop.
-		  if ("j" === firstLetter) {
-		    byeSpeaker.speak(names[i]);
-		  } else {
-		    helloSpeaker.speak(names[i]);
-		  }
+//		  if ("j" === firstLetter) {
+//		    byeSpeaker.speak(names[i]);
+//		  } else {
+//		    helloSpeaker.speak(names[i]);
+//		  }
+		
+		salutations(names[i], helloSpeaker.speak, byeSpeaker.speak);
 	  }
+	
+	names.map(salutations);
+	
+	var helloGoodbye = names.reduce(function(accumulator, element, index, array){
+		
+		var salutation = salutations(element, helloSpeaker.speakSimple, byeSpeaker.speakSimple)
+		
+		if(salutation.toLowerCase().includes("hello")){
+			accumulator.hello.push(salutation);
+		}else{
+			accumulator.goodbye.push(salutation);
+		}
+		
+		
+		return accumulator;
+	}, {hello:[], goodbye:[]});
+	
+	for(var i=0; i<helloGoodbye.hello.length; i++){
+		console.log(helloGoodbye.hello[i]);
+	}
+	
+	for(var i=0; i<helloGoodbye.goodbye.length; i++){
+		console.log(helloGoodbye.goodbye[i]);
+	}
+	
 })();
