4524bdbe309a2bcd1e911f403811e7154b776ec2
kbyrd <kbyrd@outlook.com>
2017-12-06 15:44:20 -0500

    ng-change implementation

--- module10/src/public/sign-up/sign-up.controller.js
+++ module10/src/public/sign-up/sign-up.controller.js
@@ -17,31 +17,55 @@ function SignUpController(SignUpService) {
   
   var responsePromise;
   
+  function register(){
+	  signUpCtrl.favError = "";
+	  
+	  SignUpService.savedName = signUpCtrl.firstName + " " + signUpCtrl.lastName;
+	  signUpCtrl.firstName = "";
+	  signUpCtrl.lastName = "";
+	  
+	  SignUpService.savedEmail = signUpCtrl.email;
+	  signUpCtrl.email = "";
+	  
+	  SignUpService.savedPhone = signUpCtrl.phone;
+	  signUpCtrl.phone = "";
+	  
+	  signUpCtrl.savedMsg = "Your information has been saved";
+  }
+  
+  signUpCtrl.findFav = function(){
+	  if(signUpCtrl.favItem && signUpCtrl.favItem.length === 2){
+		  responsePromise = SignUpService.findFavItemPromise(signUpCtrl.favItem.toUpperCase());
+		  
+		  responsePromise.then(function success(response){
+			  signUpCtrl.favError = "";
+			  SignUpService.savedFav = response.data;
+			  
+		  }, function error(response){
+			  signUpCtrl.favError = "No such number exists";
+		  });
+	  }
+  }
+  
   signUpCtrl.submit = function(){
 	  signUpCtrl.savedMsg = "";
-	  responsePromise = SignUpService.findFavItemPromise(signUpCtrl.favItem);
 	  
-	  responsePromise.then(function success(response){
-		  signUpCtrl.favError = "";
+	  if(!SignUpService.savedFav){
+		  responsePromise = SignUpService.findFavItemPromise(signUpCtrl.favItem.toUpperCase());
 		  
-		  SignUpService.savedName = signUpCtrl.firstName + " " + signUpCtrl.lastName;
-		  signUpCtrl.firstName = "";
-		  signUpCtrl.lastName = "";
-		  
-		  SignUpService.savedEmail = signUpCtrl.email;
-		  signUpCtrl.email = "";
-		  
-		  SignUpService.savedPhone = signUpCtrl.phone;
-		  signUpCtrl.phone = "";
-		  
-		  SignUpService.savedFav = response.data;
-		  signUpCtrl.favItem = "";
-		  
-		  signUpCtrl.savedMsg = "Your information has been saved";
-		  
-	  }, function error(response){
-		  signUpCtrl.favError = "No such number exists";
-	  });
+		  responsePromise.then(function success(response){
+			  register();
+			  
+			  SignUpService.savedFav = response.data;
+			  signUpCtrl.favItem = "";
+			  
+		  }, function error(response){
+			  signUpCtrl.favError = "No such number exists";
+		  }); 
+	  }else{
+		  register();
+	  }
+	  
   }
   
   
